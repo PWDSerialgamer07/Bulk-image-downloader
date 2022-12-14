2be6c84f67b9bb688ef77fce9efe25d6e73da86b
PWDSerialgamer07 <118306463+PWDSerialgamer07@users.noreply.github.com>
2022-12-14 15:31:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,116 +1,62 @@
-from bs4 import *
-import requests
+from imgdl import download
 import os
+from bs4 import BeautifulSoup as BSHTML
+import urllib
+def Start():
+    #Create "Albums" folder if it doesn't exist already
+    os.listdir
+    T = "Albums"
+    if T not in os.listdir():
+        os.mkdir("Albums")
+    #Check the list.txt file for duplicate with the completed.txt file.
+    with open('list.txt','r+') as f:
+        linesA = f.read().split("\n")
+    with open('completed.txt','r+') as t:
+        linesB = t.read().split("\n")
+    #Delete said duplicates in the list
+    for i in linesA[:]:
+        if i in linesB:
+            linesA.remove(i)
+    #Delete said duplicates from the .txt
+    with open('list.txt','w') as y:
+        for item in linesA:
+            y.write("%s\n" % item)
+    with open('completed.txt','w') as u:
+        for item in linesB:
+            u.write("%s\n" % item)
 
-# CREATE FOLDER
-def folder_create(images):
-	try:
-		folder_name = input("Enter Folder Name:- ")
-		# folder creation
-		os.mkdir(folder_name)
-
-	# if folder exists with that name, ask another name
-	except:
-		print("Folder Exist with that name!")
-		folder_create()
-
-	# image downloading start
-	download_images(images, folder_name)
-
-
-# DOWNLOAD ALL IMAGES FROM THAT URL
-def download_images(images, folder_name):
-
-	# initial count is zero
-	count = 0
-
-	# print total images found in URL
-	print(f"Total {len(images)} Image Found!")
-
-	# checking if images is not zero
-	if len(images) != 0:
-		for i, image in enumerate(images):
-			# From image tag ,Fetch image Source URL
-
-						# 1.data-srcset
-						# 2.data-src
-						# 3.data-fallback-src
-						# 4.src
-
-			# Here we will use exception handling
-
-			# first we will search for "data-srcset" in img tag
-			try:
-				# In image tag ,searching for "data-srcset"
-				image_link = image["data-srcset"]
-				
-			# then we will search for "data-src" in img
-			# tag and so on..
-			except:
-				try:
-					# In image tag ,searching for "data-src"
-					image_link = image["data-src"]
-				except:
-					try:
-						# In image tag ,searching for "data-fallback-src"
-						image_link = image["data-fallback-src"]
-					except:
-						try:
-							# In image tag ,searching for "src"
-							image_link = image["src"]
-
-						# if no Source URL found
-						except:
-							pass
-
-			# After getting Image Source URL
-			# We will try to get the content of image
-			try:
-				r = requests.get(image_link).content
-				try:
-
-					# possibility of decode
-					r = str(r, 'utf-8')
-
-				except UnicodeDecodeError:
-
-					# After checking above condition, Image Download start
-					with open(f"{folder_name}/images{i+1}.jpg", "wb+") as f:
-						f.write(r)
-
-					# counting number of image downloaded
-					count += 1
-			except:
-				pass
-
-		# There might be possible, that all
-		# images not download
-		# if all images download
-		if count == len(images):
-			print("All Images Downloaded!")
-			
-		# if all images not download
-		else:
-			print(f"Total {count} Images Downloaded Out of {len(images)}")
-
-# MAIN FUNCTION START
-def main(url):
-
-	# content of URL
-	r = requests.get(url)
-
-	# Parse HTML Code
-	soup = BeautifulSoup(r.text, 'html.parser')
-
-	# find all images in URL
-	images = soup.findAll('img')
-
-	# Call folder create function
-	folder_create(images)
-
-
-# take url
-url = input("Enter URL:- ")
-
-# CALL MAIN FUNCTION
-main(url)
+def Menu():
+    #Start of user activity
+    A=50
+    print("Welcome to my bad bulk image downloader. Also my first actual python project since I learned it\n1.Download from a link\n2.Download from list.txt without user input\n3.Settings")
+    inpuut = input()
+    if inpuut == "3":
+        print("1.Number of download threads(default is 50)\n2.Return")
+        V = input()
+        if V == "1":
+            A =input("Number of threads: ")
+            Menu()
+        if V == "2":
+            Menu()
+    if inpuut == "1":
+        #Fetching image URLS
+        count = "1"
+        link = input("Enter link: ")
+        page = urllib.request.urlopen(link)
+        soup = BSHTML(page)
+        images = soup.findAll('img')
+        count2 = 'Albums/Folder'+count
+        os.mkdir(count2)
+        path = count2
+        for image in images:
+            P=(image['src'])
+            try:
+                paths = download(P, store_path=path, n_workers=A)
+            except:
+                pass
+        count = count+"1"
+    if inpuut == "2":
+        print("WIP")
+        Menu()
+Start()
+Menu()
